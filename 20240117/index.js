8e92ef44cb9fb09f46b677fe27a81da3f30bd28a
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2024-01-22 13:54:15 +0100

    20240117

---
+++ 20240117/index.js
@@ -0,0 +1,11 @@
+//console.log("szia!");
+
+function osszead(num1, num2){
+    //console.log(num1+num2)
+    document.body.children[1].innerHTML=num1+num2;
+    return num1+num2;
+}
+
+var a=3;
+var b=5;
+//var c=osszead(a,b);
